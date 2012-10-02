c8511f11414fd0b40547336256368a3ab59f1bc0
Brian O'Donnell <brian.odonnell@dealer.com>
2012-10-02 07:07:25 -0400

    Added /api/get_categories

--- VTrackerAPI/lib/VTrackerAPI/Controller/API.pm
+++ VTrackerAPI/lib/VTrackerAPI/Controller/API.pm
@@ -29,12 +29,7 @@ sub index :Path :Args(0) {
     $c->response->body('Matched VTrackerAPI::Controller::API in API.');
 }
 
-sub new_key :Local :Path( '/api' ) :Args( 0 ) :ActionClass('REST') {
-	my ( $self, $c ) = @_;
-	
-#	$c->forward( 'View::JSON' );
-	
-}
+sub new_key :Local :Path( '/api' ) :Args( 0 ) :ActionClass('REST') {}
 
 sub new_key_POST {
 	my ( $self, $c ) = @_;
@@ -58,6 +53,62 @@ sub new_key_POST {
 	return $self->status_no_content( $c, message => 'ERROR' ) if not defined( $id );
 	return $self->status_created( $c, location => $c->req->uri->as_string, entity => { key => "$id" } );
 };
+
+sub get_categories :Local :Path( '/api' ) :ActionClass('REST') { }
+
+sub get_categories_GET {
+	my ( $self, $c, $params ) = @_;
+	my $cursor = $c->fetchDocuments( 'categories', {} );
+	my $categories = { categories => [] };
+	my @results = ();
+	my $param = parse_params( $params );
+		
+	$param->{'type'} = 'popular' unless defined( $param->{'type'} );
+	$param->{'sort'} = 'popular' unless defined( $param->{'sort'} );
+	$param->{'ids'} = [] unless defined( $param->{'ids'} );
+
+	# sort the resultset
+	if( $param->{'sort'} eq 'alpha' ){
+		$cursor->sort( { name => 1 } );
+	}
+	elsif( $param->{'sort'} eq 'popular' ){
+		$cursor->sort( { likes => 1 } );
+	}
+	
+	# narrow the resultset	
+	if( $param->{'type'} eq 'all' ){
+		@results = $cursor->all;
+	}
+	elsif( $param->{'type'} eq 'custom' ){
+		@results = $cursor->all;
+	}
+	elsif( $param->{'type'} eq 'popular' ){
+		@results = $cursor->limit( 10 )->all;
+	}
+	else {
+		return $self->status_not_found( $c, message => "Invalid type parameter" );
+	}
+	
+	foreach my $result ( @results ){ $result->{'_id'} = "$result->{'_id'}" };
+	$categories->{'categories'} = \@results; 
+	return $self->status_ok( $c, entity => $categories );
+}
+	
+sub parse_params :Private {
+	my ( $string ) = @_;
+	
+	my %params = ();
+	my @param_list = split( ',', $string );
+	
+	foreach my $p ( @param_list ){
+		my ( $key, $value ) = split( '=', $p );
+		
+		$value =~ s/\+/ /g;
+		$params{$key} = $value;
+	}
+	
+	return \%params;
+}
 	
 	
 
