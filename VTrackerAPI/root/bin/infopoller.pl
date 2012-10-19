acb057797fca6aeb097c8054aabb032c63fd411f
Zack Colgan <zack@uovgaz.com>
2012-10-19 19:39:42 -0400

    infopoller is working with basic lookup

--- VTrackerAPI/root/bin/infopoller.pl
+++ VTrackerAPI/root/bin/infopoller.pl
@@ -4,6 +4,7 @@ use warnings;
 use MongoDB;
 
 
+# Initialize variables, database, and collection
 my $ip = '127.0.0.1';
 my $api_key = '00000000000000000000000000000000';
 my $count = shift;
@@ -11,4 +12,11 @@ my $mongo = MongoDB::Connection->new(); # defaults to localhost with no auth
 my $database = $mongo->vtracker;
 my $reports = $database->reports;
 
-
+# Lookup records that are missing detailed location data
+my $workingset = $reports->find( { 'location.county' => { '$exists' => 0 } }, { location => 1 } );
+while(my $something = $workingset->next)
+{
+	print "LA ".$something->{'location'}->{'lat_long'}[0]."\n";
+	print "LO ".$something->{'location'}->{'lat_long'}[1]."\n";
+	print "EL ".$something->{'location'}->{'elevation'}."\n";
+}
