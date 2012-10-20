a36e9f7f6b518647db72041e866783611425a5ac
Jonah Schulte <jonah.schulte@dealer.com>
2012-10-20 12:30:25 -0400

    Map work

--- VTrackerSite/js/app/application.js.php
+++ VTrackerSite/js/app/application.js.php
@@ -218,6 +218,7 @@ $('#search').on('pageinit', function() {
 	$.ajax({
 		type: "GET",
 		data: {
+			expanded : 1,
 			// type : "",
 			// sort : "",
 			// count : "",
@@ -226,51 +227,139 @@ $('#search').on('pageinit', function() {
 		url: 'http://vtracker.hzsogood.net/api/get_reports',
 		success: function(data) {
 
-			console.log(data);
+			// We need to bind the map with the "init" event otherwise bounds will be null
+			$('#map').gmap({
+				'center': '44.260113, -72.575386',
+				'zoom': 7,
+				'disableDefaultUI': false
+			}).bind('init', function(evt, map) { 
 
-	// TODO: ON success of the json api call, do everything below this line
-	// We need to bind the map with the "init" event otherwise bounds will be null
-		$('#map').gmap({
-			'center': '44.260113, -72.575386',
-			'zoom': 7,
-			'disableDefaultUI': false
-		}).bind('init', function(evt, map) { 
+				// Vermont bounds
+				//# top left: 44.816855,-73.119176
+				//# bottom right: 43.25283,-72.468923
 
-			// Vermont bounds
-			//# top left: 44.816855,-73.119176
-			//# bottom right: 43.25283,-72.468923
+				// var bounds = map.getBounds();
+				// var southWest = bounds.getSouthWest();
+				// var northEast = bounds.getNorthEast();
+				// var lngSpan = northEast.lng() - southWest.lng();
+				// var latSpan = northEast.lat() - southWest.lat();
 
-			// var bounds = map.getBounds();
-			// var southWest = bounds.getSouthWest();
-			// var northEast = bounds.getNorthEast();
-			// var lngSpan = northEast.lng() - southWest.lng();
-			// var latSpan = northEast.lat() - southWest.lat();
+				// _id: "50827f98c8f06d8c12000000"
+				// attributes: Object
+				// 		count_female: 0
+				// 		count_juvenile: 0
+				// 		count_male: 0
+				// 		count_total: 1
+				// 		count_unknown: 1
+				// 		is_tracks: 0
+				// 		species: "Odocoileus virginianus"
+				// conditions: Object
+				// 		dewpoint_c: 12
+				// 		pressure_mb: 1004
+				// 		relative_humidity_percent: 98
+				// 		temp_c: 12.1
+				// 		uv: 0
+				// 		weather: "Clear"
+				// 		wind_degrees: 112
+				// 		wind_kph: 0
+				// 		ip_address: "127.0.0.1"
+				// location: Object
+				// 		abbr: "VT"
+				// 		country: "United States"
+				// 		county: "Windsor"
+				// 		elevation: 65.831186759603
+				// 		lat_long: Array[2]
+				// 		state: "Vermont"
+				// 		town: "Plymouth"
+				// 		zip: "05056"
+				// timestamp: Object
+				// 		day: 18
+				// 		epoch_time: 1350589809
+				// 		hour: 15
+				// 		minute: 50
+				// 		month: 10
+				// 		second: 7
+				// 		string: "Thu Oct 18 15:50:07 EDT 2012"
+				// 		timezone: -400
+				// 		year: 2012
 
-			var reports = data["reports"];
-			for ( var i = 0; i < reports.length; i++ ) {
-				var lat = reports[i].location.lat_long[0];
-				var lng = reports[i].location.lat_long[1];
-				$('#map').gmap('addMarker', { 
-					'position': new google.maps.LatLng(lat, lng) 
-				}).click(function() {
-					$('#map').gmap('openInfoWindow', { content : "<a href='http://en.wikipedia.org/w/index.php?title=Bear&action=render' class='dialog'>Read about Bears</a>" }, this);
-				});
-			}
+				var reports = data["reports"],
+					report;
 
-			$('#map').gmap('set', 'MarkerClusterer', new MarkerClusterer(map, $('#map').gmap('get', 'markers')));
-			// To call methods in MarkerClusterer simply call
-			// $('#map_canvas').gmap('get', 'MarkerClusterer').callingSomeMethod();
-		});	
+				for ( var i = 0; i < reports.length; i++ ) {
+	
+					report = reports[i];
+
+					attributes = report.attributes,
+						count_female = attributes.count_female,
+						count_juvenile = attributes.count_juvenile,
+						count_male = attributes.count_male,
+						count_total = attributes.count_total,
+						count_unknown = attributes.count_unknown,
+						is_tracks = attributes.is_tracks;
+
+					if (attributes.species) {
+						species = attributes.species;
+						current_species = species.common_names[0];
+						latin_name = species._id;
+					}
+
+					if (report.conditions) {
+						conditions = report.conditions;
+						dewpoint_c = conditions.dewpoint_c;
+						pressure_mb = conditions.pressure_mb;
+						relative_humidity_percent = conditions.relative_humidity_percent;
+						temp_c = conditions.temp_c;
+						uv = conditions.uv;
+						weather = conditions.weather;
+						wind_degrees = conditions.wind_degrees;
+						wind_kph = conditions.wind_kph;
+					}
+
+					if (report.location) {
+						loc = report.location;
+						abbr = loc.abbr;
+						country = loc.country;
+						county = loc.county;
+						elevation = loc.elevation;
+						lat = loc.lat_long[0];
+						lng = loc.lat_long[1];
+						state = loc.state;
+						town = loc.town;
+						zip = loc.zip;
+					}
+
+					if (report.timestamp) {
+						timestamp = report.timestamp;
+						day = timestamp.day;
+						epoch_time = timestamp.epoch_time;
+						hour = timestamp.hour;
+						minute = timestamp.minute;
+						month = timestamp.month;
+						second = timestamp.second;
+						string = timestamp.string;
+						timezone = timestamp.timezone;
+						year = timestamp.year;
+					}
+
+					$('#map').gmap('addMarker', { 
+						'position': new google.maps.LatLng(lat, lng) 
+					}).click(function() {
+						$('#map').gmap('openInfoWindow', { content : "<a href='/inc/getpage.inc.php?query=" + latin_name + "' data-rel='dialog' data-role='button'>Read about " + current_species + "</a>" }, this);
+					});
+				}
+
+				$('#map').gmap('set', 'MarkerClusterer', new MarkerClusterer(map, $('#map').gmap('get', 'markers')));
+				// To call methods in MarkerClusterer simply call
+				// $('#map_canvas').gmap('get', 'MarkerClusterer').callingSomeMethod();
+			});	
 
 		},
 		error: function (XMLHttpRequest, textStatus, errorThrown) {
 			console.log(XMLHttpRequest, textStatus, errorThrown);
 		}
 	});
-
-
-
-	// $(".dialog").live("click", function() {
-	// 	$.mobile.changePage($(this).attr('href'),'pop',false,true);
-	// });
+	$(".dialog").live("click", function() {
+		$.mobile.changePage($(this).attr('href'),'pop',false,true);
+	});
 });
